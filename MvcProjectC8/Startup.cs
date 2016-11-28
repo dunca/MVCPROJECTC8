14087ef64786cb885808a6e9a09c087e4de669cf
F <F@CEPUS>
2016-11-28 18:18:07 +0200

    Add project files.

---
+++ MvcProjectC8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcProjectC8.Startup))]
+namespace MvcProjectC8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
