3276deebd9dfbe4dd32658cd630818245b5838da
flvdnc <F@CEPUS>
2016-12-12 19:45:02 +0200

    ex 3

--- MvcProjectC8/Models/ProjectEvaluation.cs
+++ MvcProjectC8/Models/ProjectEvaluation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,8 +12,14 @@ namespace MvcProjectC8.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(1000)]
         public string City { get; set; }
         public string Country { get; set; }
+
+        [Required]
+        [Range(1, 10)]
         public int Rating { get; set; }
     }
 }
