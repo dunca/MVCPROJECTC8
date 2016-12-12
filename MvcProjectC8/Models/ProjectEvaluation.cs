28a6a933e5cdbff198081c1601dfeb3a158ec280
flvdnc <F@CEPUS>
2016-12-12 19:33:34 +0200

    ex 2

--- MvcProjectC8/Models/ProjectEvaluation.cs
+++ MvcProjectC8/Models/ProjectEvaluation.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MvcProjectC8.Models
 {
+    [Bind(Exclude = "Country")]
     public class ProjectEvaluation
     {
         public int Id { get; set; }
